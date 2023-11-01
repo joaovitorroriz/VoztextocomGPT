847d018dd999bc197ab9080cb8c4ba40f9674896
joao <joao.silva.17@edu.ufes.br>
2023-11-01 14:45:15 -0300

    Adicionar arquivo ubuntu

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 
 
 const startRecordingButton = document.getElementById('startRecording');
+const outputText = document.getElementById('user-input');
 
 const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)();
-recognition.lang = 'pt-BR';
+recognition.lang = "pt-BR"; // Define o idioma para o reconhecimento de fala (português brasileiro, por exemplo)
 recognition.interimResults = false;
 recognition.maxAlternatives = 1;
 
@@ -19,30 +20,51 @@ userInput.addEventListener("keyup", function(event) {
     }
 });
 
-startRecordingButton.addEventListener('click', () => {
+startRecordingButton.addEventListener("mousedown", () => {
+    startListening();
+});
+
+startRecordingButton.addEventListener("mouseup", () => {
+    stopListening();
+});
+
+function startListening() {
+    startRecordingButton.style.backgroundColor = "#f7210d";
     recognition.start();
 
     recognition.onresult = (event) => {
         const speechToText = event.results[0][0].transcript;
         outputText.textContent = `Texto Reconhecido: ${speechToText}`;
-
+        print("foi")
         // Enviar speechToText para o backend (por exemplo, via AJAX) para processamento com GPT.
         // Chame uma função que faz a requisição para a API do seu backend para processamento com GPT aqui.
     };
-
-    recognition.onerror = (event) => {
-        console.error('Erro de Reconhecimento de Fala:', event.error);
+    recognition.onerror = function(event) {
+        console.error("Erro de reconhecimento de fala: ", event.error);
     };
-});
+
+}
+
+function stopListening() {
+    startRecordingButton.style.backgroundColor = "#52ff13";
+    recognition.stop(); // Para o reconhecimento de fala
 
 
+    displayMessage(outputText)
+
+}
 
 
 
 function sendMessage() {
-    var userInput = document.getElementById("user-input").value;
+var userInput = document.getElementById("user-input").value;
+displayMessage(userInput) 
+
+}
+
+function displayMessage(userInput) {   
     var chatHistory = document.getElementById("chat-history");
-    
+
     // Adicione a mensagem do usuário ao histórico de conversas
     chatHistory.innerHTML += "<p><strong>Você:</strong> " + userInput + "</p>";
     
@@ -57,4 +79,6 @@ function sendMessage() {
     
     // Role para o final do histórico de conversas
     chatHistory.scrollTop = chatHistory.scrollHeight;
+
+
 }
