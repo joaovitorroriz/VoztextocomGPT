73f5f1ce37b3b9a6094f25acab8e46538c7b001f
joao <joao.silva.17@edu.ufes.br>
2023-12-20 00:24:27 -0300

    Mensagem do commit

--- preprocessamento.py
+++ preprocessamento.py
@@ -1,9 +1,15 @@
 from preprocessamento.preparando_base import converter_mp4_para_wav
 from preprocessamento.tratar_audio import dividir_audios
-
+from preprocessamento.tratar_legenda import criar_csv
 
 # converte audio.mp4 para audio.wav
 # converter_mp4_para_wav()
+# print("Conversão concluída")
 
 # Chamar a função de processamento
-dividir_audios()
+# dividir_audios()
+# print("Divisão concluída")
+
+# Atualiza o aquivo texts com a descrição 
+criar_csv()
+print("Atualização concluída")
---
+++ setup_env.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Configurar CUDA e cuDNN para este projeto
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
+
+# Ativar o ambiente virtual Python
+source "/home/lcad4-mestrado/Área de Trabalho/Trabalho_IA/VoztextocomGPT/venv_projet/bin/activate"
